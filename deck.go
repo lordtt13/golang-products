8a0005479563b92806a98a33e5efd9264cbf5a64
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-10-10 21:08:20 +0530

    Nested Loops Implementation Added

--- deck.go
+++ deck.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type deck []string
 
+func wholeDeck() deck{
+	cards := deck{}
+
+	suits := []string{"Hearts","Diamonds","Spades","Clubs"}
+	values := []string{"Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"}
+
+	for _,suit := range suits{
+		for _,value := range values{
+			cards = append(cards, value + " of " + suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print(){
 	for i, card := range d{
 		fmt.Println(i, card)
--- newDeck.go
+++ newDeck.go
@@ -1,12 +1,7 @@
 package main
 
 func main(){
-	cards := deck{"Two of Diamonds", newCard()}
-	cards = append(cards, "Three of Diamonds")
+	cards := wholeDeck()
 
 	cards.print()
-}
-
-func newCard() string{
-	return "Ace of Diamonds"
 }

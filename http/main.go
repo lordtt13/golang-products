2ed86481f51d0334f6eb0ed012d573438fe2bdbe
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-10-13 22:19:02 +0530

    Basic HTTP Requests Added

---
+++ http/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main(){
+	resp, err := http.Get("https://github.com/lordtt13")
+	if err != nil{
+		fmt.Println("Error: ",err)
+		os.Exit(1)
+	}
+
+	fmt.Println(resp)
+}

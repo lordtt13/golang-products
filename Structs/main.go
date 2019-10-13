e4cd238ab86695ddf3cea4ecbf21a7e2ddfe5458
Tanmay Thakur <thakurtanmay72@yahoo.com>
2019-10-13 12:58:46 +0530

    Embedded Structs Implementation Added

--- Structs/main.go
+++ Structs/main.go
@@ -2,17 +2,28 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	zip int
+}
+
 type person struct {
 	firstName string
 	lastName string
+	contact contactInfo
 }
 
 func main(){
 	anu := person{
 		firstName: "Anusha",
 		lastName:  "Gupta",
+		contact:contactInfo{
+			email: "whatever@gmail.com",
+			zip:   632014,
+		},
 	}
 
 	fmt.Println(anu)
+	fmt.Printf("%+v",anu)
 }
 

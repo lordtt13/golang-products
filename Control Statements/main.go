ed7b0ed44b7a77ca31f01252effe3c122fa8a54f
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-03-03 13:56:41 +0530

    Added switch case statements to main func
    Signed-off-by: Tanmay Thakur <thakurtanmay72@yahoo.com>

--- Control Statements/main.go
+++ Control Statements/main.go
@@ -24,5 +24,34 @@ func main() {
 	mymap["age"] = 21
 	for k, v := range mymap {
 		fmt.Printf("Key: %s and Value: %v", k, v)
+		fmt.Print(" ")
+	}
+
+	flag2 := true
+	// continue, break, switch case,
+	for i := 1; i < 10; i++ {
+		if i%2 == 0 {
+			flag2 = false
+			break
+		} else if i == 1 {
+			continue
+		}
+	}
+	fmt.Println(flag2)
+
+	day := "Fri"
+	switch day {
+	case "Fri":
+		fmt.Println("TGIF")
+	case "Mon", "Tue", "Wed":
+		fmt.Println("Boring")
+	default:
+		fmt.Println("default")
+	}
+
+	switch {
+	case day == "Fri":
+		fmt.Println("TGIF")
+		break
 	}
 }

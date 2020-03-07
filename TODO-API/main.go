fcbd27b5b22ed0d5e6f5e5b4919f83a2b6f88156
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-03-07 10:31:55 +0530

    Compiler main func add
    Signed-off-by: Tanmay Thakur <thakurtanmay72@yahoo.com>

--- TODO-API/main.go
+++ TODO-API/main.go
@@ -1,8 +1,15 @@
 package main
+
 import (
 	"fmt"
+	"log"
+	"lordtt13/GO_Prods/TODO-API/controller"
+	"lordtt13/GO_Prods/TODO-API/model"
 	"net/http"
+	"github.com/go-sql-driver/mysql"
 )
+
+/*
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/getgoing/", func(w http.ResponseWriter, r *http.Request) {
@@ -16,4 +23,13 @@ func main() {
 		w.Write([]byte("Hello world go"))
 	})
 	http.ListenAndServe("localhost:3000", mux)
+}
+*/
+
+func main() {
+	mux := controller.Register()
+	db := model.Connect()
+	defer db.Close()
+	fmt.Println("Serving...")
+	log.Fatal(http.ListenAndServe(":3000", mux))
 }
